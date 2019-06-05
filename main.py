2b3b6f7e489381e596e3bddfa581b178b0ff00cc
athoslag <athoslagemann@gmail.com>
2019-06-05 16:55:59 -0300

    Add custom input from keyboard to search lyrics

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ def printResult(pair):
 		print(result.song.name)
 		print(result.artist.name)
 		print()
-		# print(result.song.lyric)
-		# print()
+		print(result.song.lyric)
+		print()
 
 def printTranslation(pair):
 	result = lyrics.find(pair.artist, pair.song)
@@ -28,8 +28,22 @@ def printTranslation(pair):
 		print(translation.name)
 		print(translation.lyric)
 
+# MAIN
+pairs = []
 
-pairs = [SongPair('radiohead', 'there, there'), SongPair('green day', 'holiday')]
+while True:
+	try:
+		artist = input()
+		if not artist: 
+			break
+
+		song = input()
+		if not song:
+			break
+		else:
+			pairs.append(SongPair(artist, song))
+	except:
+		break
 
 for pair in pairs:
 	printResult(pair)
