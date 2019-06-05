47151791f152d5ae7b457e8416389de906180d62
athoslag <athoslagemann@gmail.com>
2019-06-05 16:33:42 -0300

    Add (artist, song) pair class, update callouts

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 from vagalume import lyrics
 
-artist_name = 'radiohead'
-song_name = 'there, there'
+class SongPair(object):
+	"""A pair containing an Artist and a Song"""
+	def __init__(self, artist, song):
+		super(SongPair, self).__init__()
+		self.artist = artist
+		self.song = song
+		
 
-result = lyrics.find(artist_name, song_name)
+def printResult(pair):
+	result = lyrics.find(pair.artist, pair.song)
+	if result.is_not_found():
+		print('Song not found')
+	else:
+		print(result.song.name)
+		print(result.artist.name)
+		print()
+		# print(result.song.lyric)
+		# print()
 
-if result.is_not_found():
-    print('Song not found')
-else:
-    print(result.song.name)
-    print(result.artist.name)
-    print()
-    print(result.song.lyric)
-    print()
-    
-    translation = result.get_translation_to('pt-br')
-    if not translation:
-        print('Translation not found')
-    else:
-        print(translation.name)
-        print(translation.lyric)
+def printTranslation(pair):
+	result = lyrics.find(pair.artist, pair.song)
+	translation = result.get_translation_to('pt-br')
+	if not translation:
+		print('Translation not found')
+	else:
+		print(translation.name)
+		print(translation.lyric)
+
+
+pairs = [SongPair('radiohead', 'there, there'), SongPair('green day', 'holiday')]
+
+for pair in pairs:
+	printResult(pair)
