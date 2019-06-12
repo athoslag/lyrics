6270f88a6715ffc6c897907c50fff9fe47a3151b
athoslag <athoslagemann@gmail.com>
2019-06-12 17:06:00 -0300

    Update requester to save formatted infos

--- requester.py
+++ requester.py
@@ -1,26 +1,46 @@
 import json
 import requests
 
-apiKey = '7011800b8ae38866c80b2afcdf5660c7'
-url = 'https://api.vagalume.com.br/radio.php'
-payload = { 'type': 'mus', 'radio': 'coca-cola-fm', 'apikey': apiKey} 
-radio = payload['radio'] + '.json'
+def saveToFile(filename, items):
+	try:
+		with open(filename, 'w') as fd:
+			for item in items:
+				fd.write(item)
+				fd.write('\n')
 
-# Performs the request
-response = requests.get(url, params=payload)
-print(response)
+		print('Successfully written to %s' % filename)
+	except:
+		print('Error: could not write to %s' % filename)
 
-try:
+def infosFromJSON(response):
+	infos = []
+	try:
+		responseJSON = response.json()
+		for music in responseJSON["mus"]:
+			artistName = music["art"]["name"]
+			musicName = music["name"]
+			infos.append(artistName + ' -> ' + musicName)
+
+		return infos
+	except:
+		print('Error: could not convert to JSON.')
+
+def main():
+	apiKey = '7011800b8ae38866c80b2afcdf5660c7'
+	url = 'https://api.vagalume.com.br/radio.php'
+	payload = { 'type': 'mus', 'radio': 'coca-cola-fm', 'apikey': apiKey} 
+	radio = payload['radio'] + '.txt'
+
+	# Performs the request
+	response = requests.get(url, params=payload)
+	print(response)
+
+	# Saves to JSON
+	data = infosFromJSON(response)
+
 	# Writes to file
-	with open(radio, 'wb') as fd:
-		for chunk in response.iter_content(chunk_size=128):
-			fd.write(chunk)
-		
-	with open(radio, 'a') as fd:
-		fd.write('\n')
+	saveToFile(radio, data)
+	print()
 
-	print('Successfully written to %s' % radio)
-	print()
-except:
-	print('JSON operation was ill-fated.')
-	print()
+if __name__ == '__main__':
+    main()
